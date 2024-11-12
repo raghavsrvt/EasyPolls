1508438cb1ef65f91932d09232079a89ad49b013
Raghav Srivastava <raghavsrivastava12102007@gmail.com>
2024-11-13 00:36:08 +0530

    removed the unnecessary use of sys module

--- src/get_absolute_path.py
+++ src/get_absolute_path.py
@@ -1,9 +1,9 @@
-import sys,os
+import os
 def resource_path(relative_path:str):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+    """ Get absolute path to resource"""
     try:
-        base_path = sys._MEIPASS
-    except Exception:
         base_path = os.path.abspath(".")
+    except:
+        print('Some error occured while retrieving resource path')        
  
     return os.path.join(base_path, relative_path)
